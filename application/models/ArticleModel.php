dd5c679dbf919cf4904e122df755248930a2ae8c
Léo Tranchet <tranchetleo@gmail.com>
2025-01-10 13:02:17 +0100

    création des models basiques

---
+++ application/models/ArticleModel.php
@@ -0,0 +1,18 @@
+<?php
+class ArticleModel extends CI_Model {
+    public function get_all_articles($limit = 15, $offset = 0) {
+        $query = $this->db->get('Article', $limit, $offset);
+        return $query->result_array();
+    }
+
+    public function get_article_by_id($id) {
+        $this->db->where('id', $id);
+        $query = $this->db->get('Article');
+        return $query->row_array();
+    }
+
+    public function create_article($data) {
+        return $this->db->insert('Article', $data);
+    }
+}
+?>
